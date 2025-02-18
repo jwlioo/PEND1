6d766bc1e030f8a6ad9d013f41bab541ae9ca78a
Júlio César Botaccio <159179841+jwlioo@users.noreply.github.com>
2025-02-18 15:55:17 -0300

    Add files via upload

---
+++ Variáveis (Lab 2.0.15).js
@@ -0,0 +1,15 @@
+let nome = "Maxwell Wright";
+let telefone = 01917196495;
+let email = "Curabitur.egestas.nunc@nonummyac.co.uk";
+
+let nome2 = "Raja Villarreal";
+let telefone2 = 08663982895;
+let email2 = "posuere.vulputate@sed.com";
+
+let nome3 = "Helen Richards";
+let telefone3 = 08001111;
+let email3 = "libero@convallis.edu;";
+
+console.log("nome 1 : "+ nome, "telefone :" + telefone, "email :" +email);
+console.log("nome 2 :"+ nome2, "telefone 2 :" + telefone2, "email 2 : " +email2);
+console.log("nome 3 :"+nome3, "telefone 3 :" + telefone3, "email 3: " +email3);
