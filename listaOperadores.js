fd014cc76ba33952f97acd5ff57c7ea2876e96ff
Júlio César Botaccio <159179841+jwlioo@users.noreply.github.com>
2025-03-25 15:56:00 -0300

    Add files via upload

---
+++ listaOperadores.js
@@ -0,0 +1,145 @@
+
+console.log("exercicio 1 :")
+console.log("")
+{
+    let x = 10;
+console.log(x); // 10
+}
+console.log("")
+//exercicio 2 //
+console.log("exercicio 2")
+console.log("")
+{
+let a = 20;
+let b= 5;
+
+console.log("addition: ", a + b);  //25 
+console.log("subtraction: ", a - b);  // 15
+console.log("multiplication: ", a * b);  // 100
+console.log("division: ", a / b);  // 4
+console.log("division remainder :", a % b); // 0
+
+
+}
+console.log("")
+// exercicio 3//
+console.log("exercicio 3")
+console.log("")
+{
+
+
+let num= -15;
+
+num = 15;
+let novoNum = num;
+}
+//exercicio 4 // 
+console.log("")
+console.log("exercicio 4:")
+console.log("")
+{
+let count = 5;
+//
+console.log(count); // -> 5
+console.log(count++); // -> 5
+console.log(count); // -> 6
+
+
+//Decrementar
+console.log(count); // -> 5
+console.log(count--); // -> 5
+console.log(count); // -> 4
+}
+
+{
+    // exercicio 5//
+    console.log("")
+console.log("exercicio 5")
+console.log("")
+let A = 10;
+
+
+A += -5;
+console.log(A); // -> 5
+A -= 3;
+console.log(A); // -> 2
+A *=2;
+console.log(A); // -> 4
+A /= 4;
+console.log(A); // -> 1 
+A **= 3;
+console.log(A); // -> 1 
+A %= 4;
+console.log(A); // ->1 
+
+}
+
+// exercicio 6//
+{
+    console.log("")
+console.log("exercicio 6")
+console.log("")
+
+let p = true;
+let q = false;
+
+console.log(p && q); // -> false
+
+console.log(p || q); // -> true
+
+console.log(!p); // -> false
+
+console.log(!q); // -> true
+
+}
+
+// exercicio 7//
+{
+    console.log("")
+console.log("exercicio 7")
+console.log("")
+
+console.log(0 || "Hello"); // hello
+console.log("" && "World"); //
+console.log(null ?? "Default Value"); //Default Value
+console.log(undefined ?? 42); // 42
+
+}
+
+// exercicio 8//
+{
+    console.log("")
+console.log("exercicio 8")
+console.log("")
+
+let resultado = 10 + 5 * 2 > 20 && !false;
+
+}
+
+// exercicio 9//
+{
+    console.log("")
+console.log("exercicio 9")
+console.log("")
+
+console.log("5" - 2); // 3
+console.log("5" + 2); // 52 
+console.log(true + 1); // 2
+console.log(false + 10); //10
+
+
+}
+
+// exercicio 10//
+{
+    console.log("")
+console.log("exercicio 10")
+console.log("")
+
+
+let x = 5;
+let y = 10;
+let z = (x++ * --y) / 2 + (y % 3);
+}
+
+
